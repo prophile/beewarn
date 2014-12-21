de1c551641216157da11039c2a732785b4bf9ce7
Alistair Lynn <arplynn@gmail.com>
2014-12-21 18:12:59 +0000

    Remove the README.md loading step

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-with open('README.md') as f:
-    description = f.read()
-
 from beewarn import VERSION
 
 setup(name='beewarn',
@@ -11,7 +8,6 @@ setup(name='beewarn',
       author='Alistair Lynn',
       author_email='arplynn@gmail.com',
       license='MIT',
-      long_description=description,
       url='https://github.com/prophile/beewarn',
       zip_safe=True,
       setup_requires=['nose >=1.0, <2.0'],
